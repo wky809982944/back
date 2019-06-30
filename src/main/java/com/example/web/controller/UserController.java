ab5f64024946d41d193568b209df001753d1fb28
鸢一折折纸 <wky809982944@126.com>
2019-06-30 18:45:28 +0800

    用户查询功能

---
+++ src/main/java/com/example/web/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable Long id) {
+        return "123";
+    }
+}
